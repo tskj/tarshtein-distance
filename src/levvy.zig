5c8206ab3868d5cce7af289ca18a712082eb0901
tskj <epost@tarjei.org>
2024-10-25 00:28:23 +0200

    simple allocation

--- src/levvy.zig
+++ src/levvy.zig
@@ -1,13 +1,34 @@
 const std = @import("std");
 
-export fn fuzzy_search(query: [*]const u8, number_of_lines: c_int, input: [*][*]const u8, output: [*]u16) callconv(.C) c_int {
-    _ = number_of_lines;
-    _ = input;
-    _ = query;
+export fn fuzzy_search(query: [*]const u8, number_of_lines: c_uint, input: [*][*]const u8, output: [*]u16) callconv(.C) c_int {
+    var q_len: usize = 0;
+    while (query[q_len] != 0) : (q_len += 1) {}
 
-    output[0] = 1;
-    output[1] = 2;
-    output[2] = 3;
+    var longest_line_length: usize = 0;
+    for (input, 0..number_of_lines) |line, index| {
+        var l_len: u16 = 0;
+        while (line[l_len] != 0) : (l_len += 1) {}
+        output[index] = l_len;
+        if (l_len > longest_line_length) longest_line_length = l_len;
+    }
+
+    // two rows are needed at a time
+    // and two tables, one for streak and one for non-streak
+    const memory_requirement = longest_line_length * 2 * 2;
+
+    const allocator = std.heap.page_allocator;
+    var buffer = allocator.alloc(u16, memory_requirement) catch {
+        return -1;
+    };
+    defer allocator.free(buffer);
+
+    buffer[0] = 10;
+    buffer[1] = 20;
+    buffer[2] = 30;
+
+    output[0] = 100;
+    output[1] = 101;
+    output[2] = 102;
 
     return 0;
 }
